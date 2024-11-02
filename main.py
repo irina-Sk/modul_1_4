28532fadf80eca4a77198d54e677622221efcf33
irina-Sk <skivfarag@gmail.com>
2024-11-03 01:56:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from multiprocessing.forkserver import get_inherited_fds
+
+my_string = input("введите текст:") # пункт 1 задания
+print(len(my_string))               # пункт 2 вывод кол-ва символов введенного текста
+
+print(my_string.upper())                      # верхний регистр
+print(my_string.lower())                      # нижний регистр
+print(my_string.replace(' ',''))  # удалить все пробелы
+print(my_string[0])                           # вывод первого символа текста
+print(my_string[-1])                          # вывод последнего символа текста
+#print(my_stritre trng)
